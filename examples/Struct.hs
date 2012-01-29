8c4b3b155ca992a483b75e87f838c882f30c18a8
Olle <ollef@hotmail.com>
2012-01-29 16:24:19 +0000

    Struct example

---
+++ examples/Struct.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE EmptyDataDecls, ScopedTypeVariables, TypeOperators #-}
+import Prelude hiding ((+), (==), (*), (-))
+import Language.C.Generate
+
+printint :: Val lr Int -> Stmt r ()
+printint n = stmt $ trustMe
+           $ "printf(\"%d\\n\", " ++ generate n ++ ")"
+
+data MyStruct
+instance StructClass MyStruct where
+  structName _ = "MyStruct"
+
+program = do
+  include "<stdio.h>"
+
+  comment "Define MyStruct"
+
+  (   (Field getx :: MyStruct :-> Int)
+   :> (Field gety :: MyStruct :-> Float)
+   ) <- defineStruct ("x" :> "y")
+
+  comment "Program entry point"
+  makeMain $ \main argc argv -> do
+    comment "Call fac with argument 5"
+    v <- newvar "v"
+    getx v =: lit 4
+    printint $ getx v
+    ret $ lit 0
+
+main = writeFile "Struct.c" $ generate program
